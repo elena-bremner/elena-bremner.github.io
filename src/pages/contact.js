2aa9fc75d7b7a66596f1c5cd386ef0822e60383e
Elena Bremner <elenabremner@risd.edu>
2020-01-12 06:27:25 -0500

    try again

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,7 +9,7 @@ export default () => (
     <a className={Styles.contactlink} href="https://www.linkedin.com/in/elena-bremner-76ab8514b/" target="_blank">  <h3>linkedin</h3> </a>
    	<a className={Styles.contactlink} href="https://www.behance.net/elenabremner" target="_blank"> <h3>behance</h3> </a>
     <a className={Styles.contactlink2} href="https://www.instagram.com/dopey.grumpy/" target="_blank"> 
-    <img src = "./contact/moccabetter.jpg" > <h3>check it out</h3> </a>
+   </a>
   </div>
   </Layout>
 )
