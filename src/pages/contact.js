1d37ad72528b945c024cee4afe5ca6aeed97b4b6
Elena Bremner <elenabremner@risd.edu>
2020-01-15 22:39:06 -0500

    spelling error

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from "../components/layout"
 import Styles from "./contact.module.css"
+import Img from "gatsby-image"
 /* eslint-disable */
 export default () => (
   <Layout>
@@ -8,7 +9,7 @@ export default () => (
     <a className={Styles.contactlink} href="mailto:ebremner@risd.edu"> <h3>email</h3> </a> 
     <a className={Styles.contactlink} href="https://www.linkedin.com/in/elena-bremner-76ab8514b/" target="_blank">  <h3>linkedin</h3> </a>
    	<a className={Styles.contactlink} href="https://www.behance.net/elenabremner" target="_blank"> <h3>behance</h3> </a>
-    <a className={Styles.contactlink2} href="https://www.instagram.com/dopey.grumpy/" target="_blank"> <h3> Check it out</h3>
+    <a className={Styles.contactlink2} href="https://www.instagram.com/dopey.grumpy/" target="_blank">  <h3>check it out</h3>
    </a>
   </div>
   </Layout>
