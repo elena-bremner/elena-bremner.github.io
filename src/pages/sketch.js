a6cf7d2a5f26343eba4a8086daf67939c2130e0b
Elena Bremner <elenabremner@risd.edu>
2020-03-25 12:22:27 +0000

    added picture to about me section and added a sketch page

---
+++ src/pages/sketch.js
@@ -0,0 +1,60 @@
+import React from "react"
+import Work from "../components/work"
+import Styles from "./sketch.module.css"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+/* this page is what you see in the UX page 
+* including name image and link to each page
+*/
+export default ({data}) => (
+  <Layout>
+  <div className={Styles.intro}>
+    <p> Welcome to my Sketches! <br></br> This is where you can find exploration and smaller projects I have worked on.  </p>
+  </div>
+
+  <div className={Styles.wrapper}> 
+  {data.allMarkdownRemark.edges.map(({ node }) =>
+      <div key={node.id} className={Styles.subwrapper}>
+        <Work
+          name={node.frontmatter.title}
+          picture= {node.frontmatter.square.childImageSharp.fluid}
+          link={"/work" + node.fields.slug}
+        />
+      </div>
+    )}
+ </div>
+ </Layout>
+
+)
+
+export const query = graphql`
+  query SQuery {
+          allMarkdownRemark (sort: { fields: [frontmatter___order], order: ASC }, filter: {frontmatter: { worktype: {eq: "sketch"} } } ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            square{
+              childImageSharp {
+                fluid(maxWidth: 1000) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            worktype
+            title
+      
+            order
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+
+  }
+`;
