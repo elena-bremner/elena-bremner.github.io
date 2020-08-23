ceb2818c1a1628b8090c04e3d39270eb43fc0258
Elena Bremner <elenabremner@risd.edu>
2020-08-23 19:29:37 +0100

    Made SIdebar component with sticky CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,63 +2,35 @@ import React from 'react'
 import { Link } from "gatsby"
 import resume from './websiteResume.pdf'
 import style from './layout.module.css'
+import NavigationIcon from '@material-ui/icons/Navigation';
+import Fab from '@material-ui/core/Fab';
+import Button from "@material-ui/core/Button";
+import { StylesProvider } from "@material-ui/core/styles";
+import Sidebar from "./sidebar";
+
+
 
 /* sidebar 
 * includes linke to each page + resume
 * wrapper contains grid
 */
-const linkStyle = {
-  color: '#CACCCE',
-  textDecoration: 'none',
-}
-const linkActiveStyle = {
-  color: "#70FFCE",  //5AC3FF
-}
 
+/**
+* StylesProvider is used to override the custom CSS over materialUI's CSS
+*/
 export default ({ children }) => {
     return (
-
-      <div className={style.container} >      
-        <div className={style.sidebar}>
-          <div className={style.ElenaBremner}>
-           Elena Bremner 
-          </div>
-          <div className = {style.work}>
-          	<Link to="/" style={linkStyle} activeStyle={linkActiveStyle}>
-              Work
-            </Link>
-          </div>
-          <br/>
-          <div className = {style.dropdown}>
-            <Link to="/UX/" className = {style.UX} style={linkStyle} activeStyle={linkActiveStyle}>
-              UX/UI
-            </Link>
-            <Link to="/CAD/" className = {style.CAD} style={linkStyle} activeStyle={linkActiveStyle}>
-              Physical 
-            </Link>
-            <Link to="/sketch/" className = {style.CAD} style={linkStyle} activeStyle={linkActiveStyle}>
-              Explorations
-            </Link>
-          </div>
-          <Link to="/about/" style={linkStyle} activeStyle={linkActiveStyle}>
-          About me
-          </Link>
-
-          <br/>
-
-          <Link to="/contact/" style={linkStyle} activeStyle={linkActiveStyle}>
-          Contact
-          </Link>
-
-          <br/> 
-
-          <a href={resume} style={linkStyle}>Resume</a>
-          <br/>
+      <StylesProvider injectFirst>
+        <div className={style.container} > 
+          <Button className={style.menu}>Menu</Button> 
+            <div className={style.sidebar}>
+            <Sidebar/>
+            </div>
+            <div className={style.maincontent}>
+              { children }
+            </div>
         </div>
-        <div className={style.maincontent}>
-          { children }
-        </div>
-      </div>
+      </StylesProvider>
     ) 
 }
  
---
+++ src/components/sidebar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import style from "./sidebar.module.css";
+import { Link } from "gatsby"
+import resume from './websiteResume.pdf'
+
+
+
+const linkStyle = {
+  color: '#CACCCE',
+  textDecoration: 'none',
+}
+const linkActiveStyle = {
+  color: "#70FFCE",  //5AC3FF
+}
+
+const Sidebar = props =>
+  <div>
+    <div className={style.ElenaBremner}>
+      Elena Bremner 
+    </div>
+    <div className = {style.work}>
+      <Link to="/" style={linkStyle} activeStyle={linkActiveStyle}>
+        Work
+      </Link>
+      <Link to="/UX/" className={style.optionalWork} style={linkStyle} activeStyle={linkActiveStyle}>
+        UX/UI
+      </Link>
+      <Link to="/CAD/" className={style.optionalWork} style={linkStyle} activeStyle={linkActiveStyle}>
+        Physical 
+      </Link>
+      <Link to="/sketch/" className={style.optionalWork} style={linkStyle} activeStyle={linkActiveStyle}>
+        Explorations
+      </Link>
+    </div>
+    <Link to="/about/" style={linkStyle} activeStyle={linkActiveStyle}>
+        About me
+    </Link>
+    <br/>
+    <Link to="/contact/" style={linkStyle} activeStyle={linkActiveStyle}>
+      Contact
+    </Link>
+    <br/> 
+      <a href={resume} style={linkStyle}>Resume</a>
+    <br/>
+</div>
+
+export default Sidebar
