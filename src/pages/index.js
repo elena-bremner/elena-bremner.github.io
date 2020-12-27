97043cc749e47a4e8316b6d87b5498f9d315d345
Elena Bremner <elenabremner@risd.edu>
2020-12-27 17:07:20 +0000

    p5.js added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Work from "../components/work"
 import style from "./index.module.css"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { Link } from "gatsby"
+import Sketch from "react-p5";
+import SketchWrapper from '../components/sketchWrapper'
+import sketch from '../scripts/sketches/shai-hulud.js'
 
 /* this page is what you see in the home page 
 * including name image and link to each page
@@ -12,24 +16,40 @@ import { graphql } from "gatsby"
 /* each node --> each md file */
 /* <work/> is a tage */
 
+   
+
+
 export default ({data}) => {
   var current_date = new Date()
   var cday = current_date.getDay()
   var weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday",
                     "Thursday", "Friday", "Saturday");
+ 
+
+ 
+
+
   return (
+  
   <Layout >
-    <div className = {style.indexContainer}>
+        <div className = {style.indexContainer}>
       <div className={style.intro}>
         <p>Happy {weekday[cday]}! </p>
        I study <a className = {style.mark}> industrial design </a> with a concentration in computation at the <a className = {style.mark}> Rhode Island School of Design</a>.
        <br></br>
        <br></br>
        <br></br>
-
-       You can reach me at ebremner@risd.edu.
-
       </div>
+
+      <div className = {style.contact}>
+        <Link to="/"  >
+              E-mail
+        </Link> 
+
+        <a href = "https://www.linkedin.com/in/elena-bremner-76ab8514b/">Linkedln</a>
+        <a href = "https://github.com/elena-bremner/elena-bremner.github.io/tree/master-source">Github</a>
+      </div>
+       <SketchWrapper sketch={sketch}/>
       <div className={style.gridWrapper}> 
         {data.allMarkdownRemark.edges.map(({ node }) =>
             <div key={node.id} className={style.squareWrapper}>
---
+++ src/sketch.js
@@ -0,0 +1,9 @@
+
+function setup() {
+  createCanvas(720, 400);
+};
+
+function draw() {
+    fill(255);
+    ellipse(100,500,100,200);
+}
