1fa908d5bdb1677489c009dab60e0a89476299c8
Elena Bremner <elenabremner@risd.edu>
2021-01-13 17:51:39 +0000

    Updates

--- component---src-templates-page-js-b4c1902e87b902b21142.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{sweJ:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),r=a.n(n),m=a("Bl7J"),l=a("LbRr");t.default=function(e){var t=e.data.markdownRemark;return r.a.createElement(m.a,null,r.a.createElement(l.a,{name:t.frontmatter.title,date:t.frontmatter.date,number:t.frontmatter.team}),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))}}}]);
-//# sourceMappingURL=component---src-templates-page-js-b4c1902e87b902b21142.js.map
