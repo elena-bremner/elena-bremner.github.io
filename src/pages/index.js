2cb3beb6aede09ecbcd535611d4a599b76e4cebe
Elena Bremner <elenabremner@risd.edu>
2021-01-16 12:41:34 +0000

    intro changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,13 +71,15 @@ export default ({data}) => {
       <div className={style.intro} data-aos="fade-up"  data-aos-delay="100">
          4th year industrial designer with a concentration in computation at the <a href = "https://www.risd.edu/"target="_blank" >Rhode Island School of Design. </a>
          <div className={style.introSnipet}>I currently work as a UI/UX intern at <a href="https://www.about.pangea.app/"target="_blank"> Pangea.app</a>, a YCombinator funded startup.</div>
+
+        <div className = {style.contact} data-aos="fade-up" data-aos-delay="200">
+            <a onClick={() => {copyTextToClipboard("ebremner@risd.edu");}}>
+               email
+            </a>
+            <a href = "https://www.linkedin.com/in/elena-bremner-76ab8514b/" target="_blank" >Linkedln</a>
+            <a href = "https://github.com/elena-bremner/elena-bremner.github.io/tree/master-source" target="_blank">Github</a>
       </div>
-      <div className = {style.contact} data-aos="fade-up" data-aos-delay="200">
-        <a onClick={() => {copyTextToClipboard("ebremner@risd.edu");}}>
-           email
-        </a>
-        <a href = "https://www.linkedin.com/in/elena-bremner-76ab8514b/" target="_blank" >Linkedln</a>
-        <a href = "https://github.com/elena-bremner/elena-bremner.github.io/tree/master-source" target="_blank">Github</a>
+
       </div>
 
       <div className={style.gridWrapper} > 
